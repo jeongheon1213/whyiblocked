36baa865f39f0c015443cf296b10eb033a3d0d9d
alal02j <alal02j@d2innovation.co.kr>
2025-12-03 18:12:12 +0900

    이미지 Get 까지 성공

--- src/main/crawler/crawler.service.js
+++ src/main/crawler/crawler.service.js
@@ -3,16 +3,36 @@ async function crawl(url, browser) {
 
     await page.setRequestInterception(true);
     page.on('request', (req) => {
-        if (['image', 'stylesheet', 'font'].includes(req.resourceType())) req.abort();
-        else req.continue();
+        req.continue();
+        // if (['image', 'stylesheet', 'font'].includes(req.resourceType())) req.abort();
+        // else req.continue();
     });
+    await page.setUserAgent(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    );
+
+    // await page.(2000);
+    // await page.mouse.move(100, 200);
 
     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
     const title = await page.title();
+    const content = await page.evaluate(() => {
+        // const elements = document.querySelectorAll('div > p');
+        // return Array.from(elements).map((el) => el.innerText.trim());
+
+        const imgs = document.querySelectorAll('.fr-view.article-content img');
+        return Array.from(imgs)
+            .map((el) => el.getAttribute('src'))
+            .filter((src) => src && src.trim() !== '');
+
+        // return document.body.innerHTML; // body 전체 텍스트
+        // return document.body; // body 전체 텍스트
+    });
+
     await page.close();
 
-    return title;
+    return content;
 }
 
 export { crawl };
--- src/views/app/app.view.js
+++ src/views/app/app.view.js
@@ -1,5 +1,13 @@
+const output = document.getElementById('output');
 document.getElementById('btn').addEventListener('click', async () => {
     const url = document.getElementById('url').value;
     const result = await window.api.crawl(url);
-    document.getElementById('output').innerText = result;
+    result.forEach((img) => {
+        const imgElem = document.createElement('img');
+        imgElem.setAttribute('src', 'https:' + img);
+        output.append(imgElem);
+    });
+    // document.getElementById('output').innerHTML = result.join('<br>');
+    console.log(result);
+    // document.getElementById('output').innerText = result;
 });
